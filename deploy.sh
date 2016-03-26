c5ccf58fba9c5b1bcfa0a9dd80e436a6cc60bfa4
Xin Xu <xinxu0x@gmail.com>
2016-03-26 18:01:28 +0800

    add fmt files

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ rm -f ~/.gitignore_global
 rm -f ~/.zshrc
 rm -rf ~/.vim
 rm -f ~/.vimrc
+rm -f ~/.htmlconf
+rm -f ~/.cssconf
+rm -f ~/.jsconf
 
 ln -s "${PWD}/_bashprofile" ~/.bash_profile
 ln -s "${PWD}/_gitconfig" ~/.gitconfig
@@ -13,3 +16,6 @@ ln -s "${PWD}/_gitignore_global" ~/.gitignore_global
 ln -s "${PWD}/_zshrc" ~/.zshrc
 ln -s "${PWD}/vim" ~/.vim
 ln -s "${PWD}/vim/_vimrc" ~/.vimrc
+ln -s "${PWD}/vim/html.json" ~/.htmlconf
+ln -s "${PWD}/vim/css.json" ~/.cssconf
+ln -s "${PWD}/vim/js.json" ~/.jsconf
